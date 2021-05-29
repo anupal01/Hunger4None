01265e8408177b62ef5b1b798d49dda3c032e17b
ManiBliss <84983331+ManiBliss@users.noreply.github.com>
2021-05-29 02:51:35 +0200

    dataengineered
    commit

---
+++ GetmulticorpFile.py
@@ -0,0 +1,43 @@
+import os
+from pathlib import Path
+import time
+import csv
+import re
+
+dirs = {}
+lst = []
+
+path='C:/Users/maniv/Downloads/multicorp'
+subdirs = [x[0] for x in os.walk(path)]
+file = open('multicorp.csv', 'w',newline='')
+with file:
+    # identifying header
+    header = ['PlantName', 'Disease', 'SetType','FileName','Path']
+    writer = csv.DictWriter(file, fieldnames = header)
+    writer.writeheader()
+    for dir in subdirs:
+        for a in Path(dir).iterdir():
+               if a.is_file():
+                    if '.rar' not in str(a):
+                        arr = str(a).split('\\')
+                        itmcnt = len(arr)
+                        spliting = arr[itmcnt - 2]
+                        nonsplit = arr[-1]
+                        if '___' in spliting:
+                            splitvalue = spliting.split('___')
+                            dirs['PlantName'] = splitvalue[0].replace(',', '')
+                            dirs['Disease'] = splitvalue[1]
+                        else:
+                            separating = (re.findall('[A-Z][^A-Z]*', nonsplit))
+                            dirs['PlantName'] = separating[0]
+                            seperatingdisease = separating[1:]
+                            dirs['Disease'] = ''.join(seperatingdisease).replace('.JPG', '')
+                        dirs['SetType'] = ''
+                        dirs['FileName'] = a.name
+                        dirs['Path'] = str(a)
+                        print(dirs)
+                    ##    time.sleep(2)
+                        ##print(dirs)
+                        writer.writerow(dirs)
+                        # time.sleep(1)
+    print('completed')
